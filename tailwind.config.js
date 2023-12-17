ae5b60f95638fb2823c5aaca7d298ee3c55fc1da
Saeed-Ayman <Saeed20359@gmail.com>
2023-12-17 10:58:35 +0300

    fix bug style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   darkMode: 'class',
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx,json}",
   ],
   theme: {
     extend: {},
